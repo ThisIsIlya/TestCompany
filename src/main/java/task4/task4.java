231d774e2e05a045627f86385405666c9a34c7c8
IlyaRo <planzet99@gmail.com>
2024-07-11 22:54:30 +0300

    задача 4

--- src/main/java/task4/task4.java
+++ src/main/java/task4/task4.java
@@ -1,8 +1,57 @@
 package task4;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static java.lang.Math.abs;
+
 public class task4 {
     public static void main(String[] args) {
 
-        String fileName = "src/main/java/task4/file1.txt";
+        if (args.length != 1) {
+            System.err.println("Ошибка: java task4 <file1>");
+            System.exit(1);
+        }
+
+        String fileName = args[0];
+
+        int numLines = 0, tmp = 0, tmp2 = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            while (br.readLine() != null) {
+                numLines++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int[] mass = new int[numLines];
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+
+            int num = 0;
+
+            while ((line = br.readLine()) != null) {
+
+                mass[num] = Integer.parseInt(line.trim());
+
+                num++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i < numLines; i++){
+            tmp +=mass[i];
+        }
+
+        int middle = tmp / numLines;
+
+        for (int i = 0; i < numLines; i++){
+            tmp2 += abs(middle - mass[i]);
+        }
+        System.out.println(tmp2);
     }
 }
